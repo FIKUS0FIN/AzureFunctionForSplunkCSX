d539a51df709eab1bdc6f039335bcfa4b4768c3f
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2018-04-06 15:45:49 +0300

    new key ..

---
+++ test.sh
@@ -0,0 +1,3 @@
+az functionapp create --deployment-source-url https://github.com/FIKUS0FIN/AzureFunctionForSplunkCSX  \
+--resource-group myNC --consumption-plan-location westeurope \
+--name func-tests --storage-account  function12121  
